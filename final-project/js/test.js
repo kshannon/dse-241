94a1091377d2ca23c64286237be30fa52c2694bb
kshannon <k.shannon.ucsd@gmail.com>
2018-03-08 22:53:44 -0800

    update test.js and circles.json

--- final-project/js/test.js
+++ final-project/js/test.js
@@ -19,32 +19,21 @@ svgCircles = svg.append("g").attr("class", "leaflet-zoom-hide"); //hide svg elem
 // choropleth = svg.append("choropleth")
 
 // set up each of these for each different svg element
-d3.json("../data/circles.json", function(error, collection) {
+d3.json("../data/test-circles.json", function(error, collection) {
     /* Add a LatLng object to each item in the dataset */
     if (error) {
         console.log(error);
         alert(error);
     }
     else {
-        var transform = d3.geoTransform({
-            point: projectPoint
-            }),
-            path = d3.geoPath().projection(transform);
-            console.log(transform)
-
-
-        var bounds = path.bounds(collection.objects),
-            topLeft = bounds[0],
-            bottomRight = bounds[1];
-            console.log(bounds[0]) //this prints out infintiey... hmmmmmmmmm
-//https://stackoverflow.com/questions/40047326/overlaying-circles-on-leaflet-with-d3-results-in-not-positioned-properly
+        var transform = d3.geoTransform({point: projectPoint});
+        var path = d3.geoPath().projection(transform);
+            // console.log(typeof path); //path is a function
 
         collection.objects.forEach(function(d) {
-            d.LatLng = new L.LatLng(d.circle.coordinates[0],
-                                    d.circle.coordinates[1]);
-            // console.log(d)
-        });
-
+            d.LatLng = new L.LatLng(d.circle[0],
+                                    d.circle[1]);
+              });
 
         var circles = svgCircles.selectAll("circle")
             .data(collection.objects)
@@ -54,33 +43,103 @@ d3.json("../data/circles.json", function(error, collection) {
             .style("fill", 'red')
             .attr('stroke', 'white')
             .attr("r", 50)
+          console.log("no");
+          console.log(circles);
 
-        function projectPoint(x, y) {
-            var point = map.latLngToLayerPoint(new L.LatLng(y, x));
-            this.stream.point(point.x, point.y);
-            console.log(point)
-        }
+        map.on("viewreset", update);
+        console.log(collection.objects);
+        update();
 
+        // Reposition the SVG to cover the features.
         function update() {
-            circles.attr("cx", function(d) {
-                return map.latLngToLayerPoint(d.LatLng).x;
-            });
-            circles.attr("cy", function(d) {
-                return map.latLngToLayerPoint(d.LatLng).y;
-            });
-            svg.attr("width", bottomRight[0] - topLeft[0])
-                .attr("height", bottomRight[1] - topLeft[1])
-                .style("left", topLeft[0] + "px")
-                .style("top", topLeft[1] + "px");
+          var bounds = path.bounds(collection.objects),
+              topLeft = bounds[0],
+              bottomRight = bounds[1];
+              console.log('.........');
+              console.log(topLeft);
+              console.log(bottomRight);
 
-            svgCircles.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+          svg.attr("width", bottomRight[0] - topLeft[0])
+              .attr("height", bottomRight[1] - topLeft[1])
+              .style("left", topLeft[0] + "px")
+              .style("top", topLeft[1] + "px");
+
+          svgCircles.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+          circles.attr("d", path);
         }
 
-      map.on("viewreset", update);
-      update();
+        // Use Leaflet to implement a D3 geometric transformation.
+        function projectPoint(x, y) {
+          var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+          this.stream.point(point.x, point.y);
+        }
 
-  }
-});
+      }
+    });
+
+
+
+
+
+        // var bounds = path.bounds(collection),
+        //     topLeft = bounds[0],
+        //     bottomRight = bounds[1];
+        //     console.log(bounds[0]); //this prints out infintiey... hmmmmmmmmm
+//https://stackoverflow.com/questions/40047326/overlaying-circles-on-leaflet-with-d3-results-in-not-positioned-properly
+
+        // collection.objects.forEach(function(d) {
+        //     d.LatLng = new L.LatLng(d.circle[0],
+        //                             d.circle[1]);
+        //     // console.log(d)
+        // });
+        // console.log(collection.objects);  // array of 4 elements
+        // console.log(typeof collection.objects); // object
+
+      //
+      //   var circles = svgCircles.selectAll("circle")
+      //       .data(collection.objects)
+      //       .enter()
+      //       .append("circle")
+      //       .style("opacity", 0.7)
+      //       .style("fill", 'red')
+      //       .attr('stroke', 'white')
+      //       .attr("r", 50)
+      //     console.log("no");
+      //     console.log(circles);
+      //
+      //   function projectPoint(x, y) {
+      //       console.log('hjhjhhhj');
+      //       console.log(y);
+      //       console.log(typeof x);
+      //       var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+      //       this.stream.point(point.x, point.y);
+      //       console.log(x);
+      //   }
+      //
+      //   function update() {
+      //     console.log('.....');
+      //       circles.attr("cx", function(d) {
+      //           return map.latLngToLayerPoint(d.LatLng).x;
+      //       });
+      //       circles.attr("cy", function(d) {
+      //           return map.latLngToLayerPoint(d.LatLng).y;
+      //       });
+      //       svg.attr("width", bottomRight[0] - topLeft[0])
+      //           .attr("height", bottomRight[1] - topLeft[1])
+      //           .style("left", topLeft[0] + "px")
+      //           .style("top", topLeft[1] + "px");
+      //
+      //       svgCircles.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+      //   }
+      //
+      // map.on("viewreset", update);
+      // update();
+      // console.log(circles);
+      // console.log("HI   !!!");
+
+//   }
+// });
 
         // var transform = d3.geoTransform({point: projectPoint}),
         //     path = d3.geoPath().projection(transform);
